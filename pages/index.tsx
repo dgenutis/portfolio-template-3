bc832d2adea301fef242387e3c134ea6d675b0b4
dgenutis <dainiusgenutis@yahoo.com>
2024-11-18 11:11:21 +0200

    nav

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import Nav from '@/Components/Nav'
+import React from 'react'
+
+const HomePage = () => {
+  return <div className='overflow-x-hidden'>
+    <div>
+      <Nav />
+    </div>
+  </div>
+  
+}
+
+export default HomePage
