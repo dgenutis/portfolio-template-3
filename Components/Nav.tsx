0249dfb5caee67c22bf9ec9a9029c329e0f66fee
dgenutis <dainiusgenutis@yahoo.com>
2024-11-19 12:12:54 +0200

    navbar + mobile navbar

--- Components/Nav.tsx
+++ Components/Nav.tsx
@@ -1,6 +1,12 @@
+import { Bars3Icon } from '@heroicons/react/16/solid'
 import React from 'react'
 
-const Nav = () => {
+interface Props {
+  openNav: () => void;
+}
+
+
+const Nav = ({openNav}:Props) => {
   return (
     <div className='w-[100%] top-0 h-[12vh] bg-[#141c27]'>
      <div className='flex items-center justify-between w-[80%] mx-auto h-[100%]'>
@@ -14,6 +20,9 @@ const Nav = () => {
         <div className='nav-link'>PROJECT</div>
         <div className='nav-link'>BLOG</div>
         <div className='nav-link'>CONTACT</div>
+        <div onClick={openNav}>
+          <Bars3Icon className='w-[2rem] md:hidden h-[2rem] cursor-pointer text-yellow-300' />
+        </div>
      </div>
     </div>
   )
