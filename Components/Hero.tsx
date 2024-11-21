85c0028c7d390681d6c0bff94e1d98e987498a2c
dgenutis <dainiusgenutis@yahoo.com>
2024-11-21 18:39:40 +0200

    textEfect + image

--- Components/Hero.tsx
+++ Components/Hero.tsx
@@ -1,13 +1,21 @@
 import React from 'react'
 import  Particle  from './Particle'
+import TextEffect from './TextEffect'
+import Image from 'next/image'
 
 const Hero = () => {
   return <div className="h-[88vh] bg-[url('/images/banner.jpg')] bg-cover bg-center">
       <Particle />
       <div className='w-[80%] grid-cols-1 mx-auto grid lg:grid-cols-2 gap-[3rem] h-[100%] items-center'>
+        <div>
     <h1 className='text-[35px] md:text-[50px] text-white font-bold'>
         HI, I'M <span className='text-yellow-400'>Dainius Genutis</span>
         </h1>
+        <TextEffect />
+        </div>
+        <div className='w-[500px] hidden bg-[#55e6a5] relative lg:flex items-center rounded-full h-[500px]'>
+            <Image src="/images/3.png" alt="user" layout='fill' className='object-cover rounded-full'   />
+        </div>
       </div>
     </div>
 
