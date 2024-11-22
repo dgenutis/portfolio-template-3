bf04a5f786965fa9e42f45e12c4ee69f827ee2c4
dgenutis <dainiusgenutis@yahoo.com>
2024-11-22 17:34:11 +0200

    hero finished

---
+++ Components/About.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const About = () => {
+  return <div className='bg-[#121121] pb-[3rem] pt-[4rem] md:pt-[8rem]'></div>
+}
+
+export default About
--- Components/Hero.tsx
+++ Components/Hero.tsx
@@ -1,24 +1,53 @@
-import React from 'react'
-import  Particle  from './Particle'
-import TextEffect from './TextEffect'
-import Image from 'next/image'
+import React from "react";
+import Particle from "./Particle";
+import TextEffect from "./TextEffect";
+import Image from "next/image";
+import { ArrowDownTrayIcon, PlayCircleIcon } from "@heroicons/react/20/solid";
 
 const Hero = () => {
-  return <div className="h-[88vh] bg-[url('/images/banner.jpg')] bg-cover bg-center">
+  return (
+    <div className="h-[88vh] bg-[url('/images/banner.jpg')] bg-cover bg-center">
       <Particle />
-      <div className='w-[80%] grid-cols-1 mx-auto grid lg:grid-cols-2 gap-[3rem] h-[100%] items-center'>
+      <div className="w-[80%] grid-cols-1 mx-auto grid lg:grid-cols-2 gap-[3rem] h-[100%] items-center">
         <div>
-    <h1 className='text-[35px] md:text-[50px] text-white font-bold'>
-        HI, I'M <span className='text-yellow-400'>Dainius Genutis</span>
-        </h1>
-        <TextEffect />
+          <h1 className="text-[35px] md:text-[50px] text-white font-bold">
+            HI, I'M <span className="text-yellow-400">Dainius Genutis</span>
+          </h1>
+          <TextEffect />
+          <p className="mt-[1.5rem] text-[18px] text-[#ffffff92]">
+            I have accumulated 18 years of experience working in a casino, from
+            a croupier to a shift manager position. Six months ago, after our
+            branch in Klaipėda closed, I decided to try something that had
+            interested me for a long time – programming. After successfully
+            completing the Vilnius Coding School JavaScript - Web Full-Stack
+            course, I realized that I would like to develop my career as a
+            junior programmer. Therefore, I am currently open to new career
+            opportunities in the programming field.
+          </p>
+          <div className="mt-[2rem] flex-col space-y-6 sm:space-y-0 sm:flex sm:flex-row items-center sm:space-x-6">
+    <button className="px-[2rem] hover:bg-yellow-400 transition-all duration-200 py-[1rem] text-[18px] font-bold uppercase bg-[#55e6a5] text-black flex items-center space-x-2">
+      <p>
+        Download Resume
+        </p>
+        <ArrowDownTrayIcon className="w-[1.6rem] h-[1.7rem] text-black" />
+    </button>
+    <button className="flex items-center space-x-2">
+      <PlayCircleIcon className="w-[4rem] h-[4rem] hover:text-yellow-400 transition-all duration-200 text-[#55e6a5]" />
+      <p className="text-[20px] font-semibold text-white">Watch the Video</p>
+    </button>
+          </div>
         </div>
-        <div className='w-[500px] hidden bg-[#55e6a5] relative lg:flex items-center rounded-full h-[500px]'>
-            <Image src="/images/3.png" alt="user" layout='fill' className='object-cover rounded-full'   />
+        <div className="w-[500px] hidden bg-[#55e6a5] relative lg:flex items-center rounded-full h-[500px]">
+          <Image
+            src="/images/3.png"
+            alt="user"
+            layout="fill"
+            className="object-cover rounded-full"
+          />
         </div>
       </div>
     </div>
+  );
+};
 
-}
-
-export default Hero
+export default Hero;
