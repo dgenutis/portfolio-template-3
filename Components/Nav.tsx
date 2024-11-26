3dd24f80512cded760ddf762c5f467a7bd173f10
dgenutis <dainiusgenutis@yahoo.com>
2024-11-26 12:57:17 +0200

    about done

--- Components/Nav.tsx
+++ Components/Nav.tsx
@@ -8,7 +8,7 @@ interface Props {
 
 const Nav = ({openNav}:Props) => {
   return (
-    <div className='w-[100%] sticky z-[10000] top-0 h-[12vh] bg-[#141c27]'>
+    <div className='w-[100%] fixed z-[10000] top-0 h-[12vh] bg-[#141c27]'>
      <div className='flex items-center justify-between w-[80%] mx-auto h-[100%]'>
         <h1 className='flec-[0.6] cursor-pointer text-[25px] text-white font-bold'>
             WEB
