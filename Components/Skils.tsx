9d18f05effcd30b7ed636eee30827309420774ce
dgenutis <dainiusgenutis@yahoo.com>
2024-12-09 14:04:21 +0200

    skills done

--- Components/Skils.tsx
+++ Components/Skils.tsx
@@ -1,12 +1,41 @@
 import React from 'react'
+import SkilsItem from './SkilsItem'
+import SkilsLanguage from './SkilsLanguage'
 
 const Skils = () => {
   return (
-    <div className='pt-[4rem] md:pt-[8rem] pb-[5rem] bg-[#09101a]'>
-      <h1 className='heading'>Education & <span className='text-yellow-400'>Skill</span></h1>
-      
+    <div className="pt-[4rem] md:pt-[8rem] pb-[5rem] bg-[#09101a]">
+      <h1 className="heading">
+        Education & <span className="text-yellow-400">Skill</span>
+      </h1>
+      <div className="w-[80%] mx-auto pt-[4rem] md:pt-[8rem] grid grid-cols-1 md:grid-cols-2 gap-[2rem] items-center">
+        <div>
+          <SkilsItem title="React Developer" year="2022 - 2023" />
+          <SkilsItem title="Mern Stack Developer" year="2023 - 2024" />
+          <SkilsLanguage
+            skill1="html"
+            skill2="css"
+            skill3="javascript"
+            level1="w-[91%]"
+            level2="w-[88%]"
+            level3="w-[80%]"
+          />
+        </div>
+        <div>
+          <SkilsItem title="Next Js Developer" year="2022 - 2023" />
+          <SkilsItem title="Node Js Developer" year="2023 - 2024" />
+          <SkilsLanguage
+            skill1="React Js"
+            skill2="Next Js"
+            skill3="Typescript"
+            level1="w-[80%]"
+            level2="w-[70%]"
+            level3="w-[60%]"
+          />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Skils
--- Components/SkilsItem.tsx
+++ Components/SkilsItem.tsx
@@ -1,7 +1,21 @@
 import React from "react";
 
-const SkilsItem = () => {
-  return <div>item</div>;
+interface Props {
+  title: string;
+  year: string;
+}
+const SkilsItem = ({title, year}: Props) => {
+  return <div className="mb-[4rem] md:mb-[8rem]">
+  <span className="px-[2rem] text-[#55e6a5] py-[0.9rem] font-bold text-[18px] border-[2px] border-[#55e6a5]">
+    {year}
+  </span>
+  <h1 className="mt-[2rem] uppercase font-semibold mb-[1rem] text-[20px] sm:text-[25px] md:text-[30px] text-white">
+    {title}
+  </h1>
+  <p className="text-[#aaaaaa] font-normal w-[80%] text-[17px] opacity-80">
+    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda natus dicta delectus animi veritatis. Unde, dolorem modi. Nostrum temporibus tenetur magnam cum sed, nesciunt commodi consequatur eos fugit soluta? Vero?
+  </p>
+  </div>;
 };
 
 export default SkilsItem;
