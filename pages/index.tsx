d86ad348d9a63f736001da8210d8cf8b94b9cfc3
dgenutis <dainiusgenutis@yahoo.com>
2024-12-15 15:00:58 +0200

    started footer

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import About from "@/Components/About";
 import Blog from "@/Components/Blog";
+import Footer from "@/Components/Footer";
 import Hero from "@/Components/Hero";
 import MobileNav from "@/Components/MobileNav";
 import Nav from "@/Components/Nav";
@@ -27,6 +28,7 @@ const HomePage = () => {
           <Projects />
           <Testimonial />
           <Blog />
+          <Footer />
         </div>
       </div>
     </div>
